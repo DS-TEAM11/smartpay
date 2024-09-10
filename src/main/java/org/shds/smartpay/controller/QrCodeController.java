61350ec5a95c0c4dd2d129e92a3f298215e9e6a1
chomyeonggeun <lightkeun@gmail.com>
2024-09-10 16:32:40 +0900

    fi: DNS url #3

--- src/main/java/org/shds/smartpay/controller/QrCodeController.java
+++ src/main/java/org/shds/smartpay/controller/QrCodeController.java
@@ -36,7 +36,7 @@ public class QrCodeController {
     @GetMapping("/seller")
     public ResponseEntity<byte[]> sendSeller(@RequestParam String memberNo) throws Exception{
         //쿼리스트링으로 memberNo 받아서 처리하게 수정
-        String url = "http://ec2-3-36-35-102.ap-northeast-2.compute.amazonaws.com:3000seller?memberNo=" + memberNo;
+        String url = "http://ec2-3-36-35-102.ap-northeast-2.compute.amazonaws.com:3000/seller?memberNo=" + memberNo;
 
         // URL을 200x200 크기의 QR 코드로 인코딩하여 BitMatrix 객체로 반환
         BitMatrix encode = new MultiFormatWriter()
